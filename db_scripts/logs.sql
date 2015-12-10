c64ecdb6a958d145929412dd71433691c60befe0
AllenKO <allenko@sjtu.edu.cn>
2015-12-10 14:33:04 +0800

    UPDATE:更新logs数据库脚本

--- db_scripts/logs.sql
+++ db_scripts/logs.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50624
 File Encoding         : 65001
 
-Date: 2015-12-10 13:49:01
+Date: 2015-12-10 14:30:46
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,15 +20,11 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `sqllog`;
 CREATE TABLE `sqllog` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `uid` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `dbname` varchar(16) NOT NULL,
   `sqlstatement` varchar(255) NOT NULL,
   `parameters` varchar(255) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
   `status` tinyint(3) unsigned zerofill NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
-
--- ----------------------------
--- Records of sqllog
--- ----------------------------
